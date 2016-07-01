037ebf8b20f833c1b35ac4f2d8bd7e363d5181f7
Andrew Hassing <hassiar@huskers.unl.edu>
2016-07-01 03:19:02 +0000

    Cucumber tests

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,7 @@
 class MoviesController < ApplicationController
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
 
   def show
     id = params[:id] # retrieve movie ID from URI route
@@ -15,7 +18,7 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.create!(params[:movie])
+    @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
