3e2e56a2da90344b737654a44fe6ce81a807dbe7
Andrew Hassing <hassiar@huskers.unl.edu>
2016-07-10 23:56:08 +0000

    Added director

---
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -0,0 +1,12 @@
+# Add a declarative step here for populating the DB with movies.
+
+Given /the following movies exist/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    Movie.create movie
+  end
+end
+
+Then /^the director of "(.*?)" should be "(.*?)"$/ do |movie_title, new_director|
+  movie = Movie.find_by_title movie_title
+  movie.director.should == new_director
+end
